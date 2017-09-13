c77bbb054007b2b950f7220da80066ee32e8123c
Connor Cairns <connor@connorcairns.xyz>
2017-09-13 21:50:17 +0100

    Database works

--- adduser.py
+++ adduser.py
@@ -5,8 +5,9 @@ c = conn.cursor()
 def addItem():
     name = input("Please enter your full name: ")
     userName = input("Please enter your username: ")
-    c.execute("""INSERT INTO student (name,userName) 
-                 VALUES(?,?)""",(name,userName,)) #putting items into the database
+    c.execute("""INSERT INTO student 
+                (name,userName)values(?,?)""",
+                (name,userName,)) #putting items into the database
     conn.commit() #commiting the changes
 
 addItem()
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from random import randint
 from num2words import num2words
 from easygui import *
+import sqlite3
 
-correct = 0
-incorrect = 0
+conn = sqlite3.connect("users.db")
+c = conn.cursor()
+
+correctQ = 0
+incorrectQ = 0
 words = ""
 number = 0
 x = 0
@@ -16,23 +20,46 @@ def question():
 
 
 def main():
-    global correct
-    global incorrect
+    global correctQ
+    global incorrectQ
     question()
-    input = enterbox(msg="Enter the number as an integer \n"+str(words), title="num2words")
-    if int(input) == number:
-        correct += 1
+    userInput = enterbox(msg="Enter the number as an integer \n"+str(words), title="num2words")
+    if int(userInput) == number:
+        correctQ += 1
         msgbox(msg="Correct!")
+        correct = True
+        c.execute("""INSERT INTO results (question,answer,correct,studentID) 
+                    VALUES(?,?,?,?)""",(words,userInput,correct,finalID))
+        conn.commit()
     else:
-        incorrect += 1
+        incorrectQ += 1
         msgbox(msg="Incorrect!")
+        correct = False
+        c.execute("""INSERT INTO results (question,answer,correct,studentID) 
+                    VALUES(?,?,?,?)""",(words,userInput,correct,finalID))
+        conn.commit()
+
+def login():
+    global finalID
+    username = enterbox(msg="Please enter your username")
+    c.execute("SELECT ID FROM student WHERE userName=?", (username,))
+    rows = c.fetchall()
+    studentID = rows[0]
+    studentID = list(map(int, studentID))
+    finalID = studentID[0]
+    loop()
 
 
-for x in range(0, 10):
-    main()
+def loop():
+    for x in range(0, 10):
+        main()
 
-else:
-    percent = (int(correct) / 10)*100
-    msgbox(percent)
+    else:
+        percent = (int(correctQ) / 10)*100
+        msgbox(percent)
+
+
+
+login()
 
 
